d98f3bb45603443138fe0914aa20eb5b4dac8de7
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-08-21 14:52:34 -0300

    Add Home View

---
+++ lib/views/home.view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:todos/widgets/navbar.widget.dart';
+import 'package:todos/widgets/todo-list.widget.dart';
+import 'package:todos/widgets/user-card.widget.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          UserCard(),
+          Navbar(),
+          Expanded(
+            child: Container(
+              child: TodoList(),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}

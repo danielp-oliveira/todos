d7063639e794e5e95515815c8aa0d41f53793c14
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-08-25 15:45:50 -0300

    Config HTTPS

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todos/stores/app.store.dart';
 import 'package:todos/themes/app.theme.dart';
 import 'package:todos/views/login.view.dart';
 
+// Esta classe permite acesso ao LocalHost com certificados HTTPS inválidos
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    HttpClient client = super.createHttpClient(context);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => true;
+    return client;
+  }
+}
+
 void main() {
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(MyApp());
 }
 

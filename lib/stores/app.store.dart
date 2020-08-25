b8ab8069ffa3f3259913c0dd69570ee9ebd598a5
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-08-25 15:05:20 -0300

    Add Observable and Action to AppStore

--- lib/stores/app.store.dart
+++ lib/stores/app.store.dart
@@ -1,23 +1,33 @@
+import 'package:mobx/mobx.dart';
 import 'package:todos/models/todo.model.dart';
 
 class AppStore {
+  @observable
   String currentState = 'none';
-  bool busy = false;
-  List<TodoItem> todos = new List<TodoItem>();
 
+  @observable
+  bool busy = false;
+
+  @observable
+  ObservableList<TodoItem> todos = new ObservableList<TodoItem>();
+
+  @action
   void changeSelected(String state) {
     currentState = state;
   }
 
+  @action
   void add(TodoItem item) {
     todos.add(item);
   }
 
+  @action
   void setTodos(List<TodoItem> items) {
     todos.addAll(items);
   }
 
+  @action
   void clearTodos() {
-    todos = new List<TodoItem>();
+    todos = new ObservableList<TodoItem>();
   }
 }

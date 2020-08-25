fe5275c34495d65a473f4e62d57a3ccab9979a27
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-08-25 15:34:25 -0300

    Add MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todos/stores/app.store.dart';
 import 'package:todos/themes/app.theme.dart';
 import 'package:todos/views/login.view.dart';
 
@@ -9,11 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todos',
-      debugShowCheckedModeBanner: false,
-      theme: appTheme(),
-      home: LoginView(),
+    return MultiProvider(
+      providers: [
+        Provider<AppStore>.value(
+          value: AppStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Todos',
+        debugShowCheckedModeBanner: false,
+        theme: appTheme(),
+        home: LoginView(),
+      ),
     );
   }
 }
